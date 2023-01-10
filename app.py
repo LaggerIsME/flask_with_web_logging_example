89674c2923c8e731067030cd741becee159503f2
hecker <pythondelay@gmail.com>
2023-01-10 15:02:06 +0600

    MongoDB connection

--- app.py
+++ app.py
@@ -1,22 +1,36 @@
 from flask import Flask, g
-from flask_sqlalchemy import SQLAlchemy
+#from flask_sqlalchemy import SQLAlchemy
+from flask_mongoengine import MongoEngine
 from flask_track_usage import TrackUsage
-from flask_track_usage.storage.sql import SQLStorage
+#from flask_track_usage.storage.sql import SQLStorage
+from flask_track_usage.storage.mongo import MongoEngineStorage
 
 app = Flask(__name__)
 
 
 # PostgreSQL настройка
 # Пример: dialect+driver://username:password@host:port/database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:12345678@localhost:5432/database_name'
+
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:12345678@localhost:5432/database_name'
+#sql_db = SQLAlchemy(app)
+
+# MongoDB настройка
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'example_website',
+    'host': 'localhost',
+    'port': 27017
+}
+db = MongoEngine()
+db.init_app(app)
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-sql_db = SQLAlchemy(app)
 
 
 with app.app_context():
     # TrackUsage
-    pstore = SQLStorage(db=sql_db)
-    t = TrackUsage(app, pstore)
+    #pstore = SQLStorage(db=sql_db)
+    mstore = MongoEngineStorage()
+    t = TrackUsage(app, mstore)
 
 
 @app.route('/')
